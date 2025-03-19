e18d63a372ca88c20ceaf91a9f22c59892c2bf48
lyszt <kalliddel@gmail.com>
2025-03-19 17:45:27 -0300

    Push exercises

--- Experiments/File Management/Exercicio A/main.c
+++ Experiments/File Management/Exercicio A/main.c
@@ -16,20 +16,53 @@
 
 int word_counter(char* filename){
     int counter;
+    printf("Contando palavras...\n");
     counter = 0;
-    FILE *f = fopen("input.txt", "r");
+    FILE *f = fopen(filename, "r");
     while(!feof(f)){
           if(fgetc(f) == ' '){
               ++counter;
           }
     }
     counter += 1; // EOF
+    fclose(f);
     return counter;
+}
+int char_counter(char* filename){
+    int counter;
+    char c;
+    printf("Contando caracteres...\n");
+    counter = 0;
+    FILE *f = fopen(filename, "r");
+    while((c = fgetc(f)) != EOF){
+        counter++;
+    }
+    printf("Terminou contagem de caracteres...");
+    fclose(f);
+    return counter;
+}
+void copyTo(char *origin, char *destination){
+      printf("Copiando input para output...\n");
+
+      // 1. Contagem de palavras
+      int file_len = char_counter(origin);
+      FILE *fdestination = fopen(destination, "w+");
+      FILE *forigin = fopen(origin, "r");
+
+      // 2. Cópia de arquivo para destino
+      char output_string[32];
+      fgets(output_string, file_len, forigin); // Indice começa em 0
+      fputs(output_string, fdestination);
+      fclose(fdestination);
+      fclose(forigin);
 }
 
 int main(){
   int word_count = word_counter("input.txt");
   printf("Número de palavras no arquivo: %i\n", word_count);
+  copyTo("input.txt","output.txt");
+
+  return 0;
 
 
 }
