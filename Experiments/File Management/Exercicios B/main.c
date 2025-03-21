e052615c79d949edb54b860ce0d50ac5d24ec860
lyszt <kalliddel@gmail.com>
2025-03-21 14:49:56 -0300

    List B of exercises

--- Experiments/File Management/Exercicios B/main.c
+++ Experiments/File Management/Exercicios B/main.c
@@ -35,8 +35,39 @@ void reverseFile(char* filename){
 
   }
 
+ void writeBinary(char* filename){
+   FILE *f = fopen(filename, "wb+");
+   int numbers[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8 ,9 , 10};
+   fwrite(numbers, sizeof(int), 11, f);
+   fclose(f);
+ }
+
+ void getBinary(char *filename, int* destination, int size){
+   FILE *f = fopen(filename, "rb");
+   int reading[size];
+   fread(destination, sizeof(int), size, f);
+ }
+void calculateAverage(int *array, int size) {
+  double total = 0;
+  printf("%d\n", size);
+  for(int i = 0; i < size; i++)
+   {
+      total += array[i];
+   }
+   double average = (total / size);
+  printf("A média é: %.2lf\n", average);
+}
 int main() {
   char* input = INPUT;
-  reverseFile(input);
+  // .1
+  // reverseFile(input);
+  // . 2 binary
+  writeBinary("binaryInput.bin");
+  int num_list[11];
+  getBinary("binaryInput.bin", num_list, 11);
+  calculateAverage(num_list, 11);
+  int testing[3] = {1,2,3};
+  calculateAverage(testing, 3);
+
   return 0;
 }
